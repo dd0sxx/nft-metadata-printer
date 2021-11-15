1513c2be369840c4b31a1a785c643e55667ce3ba
dd0sxx <contacttheotelonis@gmail.com>
2021-11-15 16:53:31 -0500

    added title and image

--- index.ts
+++ index.ts
@@ -7,22 +7,32 @@ h1.innerText = 'NFT Metadata Generator';
 const image = document.createElement('div')
 const title = document.createElement('div')
 const description = document.createElement('div')
+const imageFormWrapper = document.createElement('div')
+const imageForm = document.createElement('form')
+const imageTextArea = document.createElement('textarea')
+const titleFormWrapper = document.createElement('div')
+const titleForm = document.createElement('form')
+const titleTextArea = document.createElement('textarea')
 const descriptionFormWrapper = document.createElement('div')
 const descriptionForm = document.createElement('form')
-const descriptiontextArea = document.createElement('textarea')
+const descriptionTextArea = document.createElement('textarea')
 
 image.innerText = 'ipfs hash:'
 title.innerText = 'title:'
 description.innerText = 'description:'
+imageFormWrapper.className = 'imageFormWrapper'
+imageTextArea.className = 'imageTextArea'
+titleFormWrapper.className = 'titleFormWrapper'
+titleTextArea.className = 'titleTextArea'
 descriptionFormWrapper.className = 'descriptionFormWrapper'
-descriptiontextArea.className = 'inputText'
+descriptionTextArea.className = 'descriptiontextArea'
 
 document.body.appendChild(h1)
 document.body.appendChild(description)
-document.body.appendChild(formWrapper)
-formWrapper.appendChild(form)
-form.appendChild(textarea1)
+document.body.appendChild(descriptionFormWrapper)
+descriptionFormWrapper.appendChild(descriptionForm)
+descriptionForm.appendChild(descriptionTextArea)
 
-textarea1.addEventListener("input", (e) => {
-    console.log('brr: ', textarea1.value)
+descriptionTextArea.addEventListener("input", (e) => {
+    console.log('description: ', descriptionTextArea.value)
 })
