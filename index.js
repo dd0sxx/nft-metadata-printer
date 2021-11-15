551e38b32ca50a5655cc3ccad425c3501eb1c33d
dd0sxx <contacttheotelonis@gmail.com>
2021-11-15 17:08:00 -0500

    added sub container

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var root = document.querySelector('#root');
+var titleState, imageState, descriptionState;
 var h1 = document.createElement('h1');
 h1.innerText = 'NFT Metadata Generator';
+var container = document.createElement('div');
+container.className = 'container';
+var subContainer1 = document.createElement('div');
+subContainer1.className = 'subContainer1';
+var subContainer2 = document.createElement('div');
+subContainer2.className = 'subContainer2';
 var image = document.createElement('div');
 var title = document.createElement('div');
 var description = document.createElement('div');
@@ -21,12 +28,29 @@ imageTextArea.className = 'imageTextArea';
 titleFormWrapper.className = 'titleFormWrapper';
 titleTextArea.className = 'titleTextArea';
 descriptionFormWrapper.className = 'descriptionFormWrapper';
-descriptionTextArea.className = 'descriptiontextArea';
+descriptionTextArea.className = 'descriptionTextArea';
 document.body.appendChild(h1);
-document.body.appendChild(description);
-document.body.appendChild(descriptionFormWrapper);
+document.body.appendChild(container);
+container.appendChild(subContainer1);
+container.appendChild(subContainer2);
+subContainer1.appendChild(title);
+subContainer1.appendChild(titleFormWrapper);
+titleFormWrapper.appendChild(titleForm);
+titleForm.appendChild(titleTextArea);
+subContainer1.appendChild(image);
+subContainer1.appendChild(imageFormWrapper);
+imageFormWrapper.appendChild(imageForm);
+imageForm.appendChild(imageTextArea);
+subContainer1.appendChild(description);
+subContainer1.appendChild(descriptionFormWrapper);
 descriptionFormWrapper.appendChild(descriptionForm);
 descriptionForm.appendChild(descriptionTextArea);
 descriptionTextArea.addEventListener("input", function (e) {
-    console.log('description: ', descriptionTextArea.value);
+    // descriptionTextArea.value
+});
+imageTextArea.addEventListener("input", function (e) {
+    // imageTextArea.value
+});
+titleTextArea.addEventListener("input", function (e) {
+    // titleTextArea.value
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 const root = document.querySelector('#root')
 
+let titleState, imageState, descriptionState
+
 const h1 = document.createElement('h1');
 h1.innerText = 'NFT Metadata Generator';
 
+const container = document.createElement('div')
+container.className = 'container'
+const subContainer1 = document.createElement('div')
+subContainer1.className = 'subContainer1'
+const subContainer2 = document.createElement('div')
+subContainer2.className = 'subContainer2'
 
 const image = document.createElement('div')
 const title = document.createElement('div')
@@ -25,14 +33,33 @@ imageTextArea.className = 'imageTextArea'
 titleFormWrapper.className = 'titleFormWrapper'
 titleTextArea.className = 'titleTextArea'
 descriptionFormWrapper.className = 'descriptionFormWrapper'
-descriptionTextArea.className = 'descriptiontextArea'
+descriptionTextArea.className = 'descriptionTextArea'
 
 document.body.appendChild(h1)
-document.body.appendChild(description)
-document.body.appendChild(descriptionFormWrapper)
+document.body.appendChild(container)
+container.appendChild(subContainer1)
+container.appendChild(subContainer2)
+subContainer1.appendChild(title)
+subContainer1.appendChild(titleFormWrapper)
+titleFormWrapper.appendChild(titleForm)
+titleForm.appendChild(titleTextArea)
+subContainer1.appendChild(image)
+subContainer1.appendChild(imageFormWrapper)
+imageFormWrapper.appendChild(imageForm)
+imageForm.appendChild(imageTextArea)
+subContainer1.appendChild(description)
+subContainer1.appendChild(descriptionFormWrapper)
 descriptionFormWrapper.appendChild(descriptionForm)
 descriptionForm.appendChild(descriptionTextArea)
 
 descriptionTextArea.addEventListener("input", (e) => {
-    console.log('description: ', descriptionTextArea.value)
+    // descriptionTextArea.value
+})
+
+imageTextArea.addEventListener("input", (e) => {
+    // imageTextArea.value
+})
+
+titleTextArea.addEventListener("input", (e) => {
+    // titleTextArea.value
 })
