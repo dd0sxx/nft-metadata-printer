2404e9bd8946006ba8f41f347d409d83c7a16244
dd0sxx <contacttheotelonis@gmail.com>
2021-11-15 10:47:17 -0500

    reg js sucks lol

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hellow console');
+var root = document.querySelector('#root');
+var h1 = document.createElement('h1');
+h1.innerText = 'NFT Metadata Generator';
+var formWrapper = document.createElement('div');
+var form = document.createElement('form');
+formWrapper.className = 'form';
+var inputText = document.createElement('input');
+inputText.className = 'inputText';
+document.body.appendChild(h1);
+document.body.appendChild(formWrapper);
+formWrapper.appendChild(form);
+form.appendChild(inputText);
--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('hellow console')
+const root = document.querySelector('#root')
+
+const h1 = document.createElement('h1');
+h1.innerText = 'NFT Metadata Generator';
+
+const formWrapper = document.createElement('div')
+const form = document.createElement('form')
+formWrapper.className = 'form'
+const inputText = document.createElement('input')
+inputText.className = 'inputText'
+
+document.body.appendChild(h1)
+document.body.appendChild(formWrapper)
+formWrapper.appendChild(form)
+form.appendChild(inputText)
+
