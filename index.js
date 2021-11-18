9f6c864d999aadcb8dd9ea893691c43138eef26e
dd0sxx <contacttheotelonis@gmail.com>
2021-11-18 13:30:06 -0500

    attributes working

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var root = document.querySelector('#root');
-var titleState, imageState, descriptionState;
+var jsonObject = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: []
+};
 var h1 = document.createElement('h1');
 h1.innerText = 'NFT Metadata Generator';
 var container = document.createElement('div');
@@ -9,34 +14,45 @@ subContainer1.className = 'subContainer1';
 var subContainer2 = document.createElement('div');
 subContainer2.className = 'subContainer2';
 var image = document.createElement('div');
-var title = document.createElement('div');
+var nameDiv = document.createElement('div');
 var description = document.createElement('div');
+var attributes = document.createElement('div');
 var imageFormWrapper = document.createElement('div');
 var imageForm = document.createElement('form');
 var imageTextArea = document.createElement('textarea');
-var titleFormWrapper = document.createElement('div');
-var titleForm = document.createElement('form');
-var titleTextArea = document.createElement('textarea');
+var nameFormWrapper = document.createElement('div');
+var nameForm = document.createElement('form');
+var nameTextArea = document.createElement('textarea');
 var descriptionFormWrapper = document.createElement('div');
 var descriptionForm = document.createElement('form');
 var descriptionTextArea = document.createElement('textarea');
-image.innerText = 'ipfs hash:';
-title.innerText = 'title:';
+var attributesFormWrapper = document.createElement('div');
+var attributesForm = document.createElement('form');
+var attributesTextArea = document.createElement('textarea');
+var jsonDisplay = document.createElement('div');
+image.innerText = 'link to image: (i.e. ipfs.io/ipfs/<hash>/<x>.png)';
+nameDiv.innerText = 'name:';
 description.innerText = 'description:';
+attributes.innerText = 'attributes: (i.e. <color: red, size: large>)';
+attributes.className = 'attributes';
 imageFormWrapper.className = 'imageFormWrapper';
 imageTextArea.className = 'imageTextArea';
-titleFormWrapper.className = 'titleFormWrapper';
-titleTextArea.className = 'titleTextArea';
+nameFormWrapper.className = 'nameFormWrapper';
+nameTextArea.className = 'nameTextArea';
 descriptionFormWrapper.className = 'descriptionFormWrapper';
 descriptionTextArea.className = 'descriptionTextArea';
+attributesFormWrapper.className = 'attributesFormWrapper';
+attributesTextArea.className = 'attributesTextArea';
+jsonDisplay.className = 'jsonDisplay';
+jsonDisplay.innerText = JSON.stringify(jsonObject);
 document.body.appendChild(h1);
 document.body.appendChild(container);
 container.appendChild(subContainer1);
 container.appendChild(subContainer2);
-subContainer1.appendChild(title);
-subContainer1.appendChild(titleFormWrapper);
-titleFormWrapper.appendChild(titleForm);
-titleForm.appendChild(titleTextArea);
+subContainer1.appendChild(nameDiv);
+subContainer1.appendChild(nameFormWrapper);
+nameFormWrapper.appendChild(nameForm);
+nameForm.appendChild(nameTextArea);
 subContainer1.appendChild(image);
 subContainer1.appendChild(imageFormWrapper);
 imageFormWrapper.appendChild(imageForm);
@@ -45,12 +61,54 @@ subContainer1.appendChild(description);
 subContainer1.appendChild(descriptionFormWrapper);
 descriptionFormWrapper.appendChild(descriptionForm);
 descriptionForm.appendChild(descriptionTextArea);
+subContainer1.appendChild(attributes);
+subContainer1.appendChild(attributesFormWrapper);
+attributesFormWrapper.appendChild(attributesForm);
+attributesForm.appendChild(attributesTextArea);
+subContainer2.appendChild(jsonDisplay);
 descriptionTextArea.addEventListener("input", function (e) {
-    // descriptionTextArea.value
+    console.log(descriptionTextArea.value);
+    jsonObject.description = descriptionTextArea.value;
+    jsonDisplay.innerText = JSON.stringify(jsonObject);
 });
 imageTextArea.addEventListener("input", function (e) {
-    // imageTextArea.value
+    jsonObject.image = imageTextArea.value;
+    jsonDisplay.innerText = JSON.stringify(jsonObject);
 });
-titleTextArea.addEventListener("input", function (e) {
-    // titleTextArea.value
+nameTextArea.addEventListener("input", function (e) {
+    jsonObject.name = nameTextArea.value;
+    jsonDisplay.innerText = JSON.stringify(jsonObject);
+});
+attributesTextArea.addEventListener("input", function (e) {
+    var string = attributesTextArea.value;
+    var length = attributesTextArea.value.length;
+    var res = [];
+    var tempObj = {};
+    var key = '';
+    var value = '';
+    var kOrV = true;
+    if (length > 0) {
+        for (var i = 0; i < length; i++) {
+            console.log(key);
+            if (string[i] == ',') {
+                tempObj[key] = value;
+                kOrV = !kOrV;
+                res.push(tempObj);
+                key = '';
+                value = '';
+                tempObj = {};
+                i++;
+            }
+            else if (string[i] == ':') {
+                tempObj[key] = null;
+                kOrV = !kOrV;
+                i++;
+            }
+            else {
+                kOrV ? key += string[i] : value += string[i];
+            }
+        }
+        jsonObject.attributes = res;
+        jsonDisplay.innerText = JSON.stringify(jsonObject);
+    }
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 const root = document.querySelector('#root')
 
-let titleState, imageState, descriptionState
+let jsonObject = {
+    name: "",
+    description: "",
+    image: "",
+    attributes: []
+}
 
 const h1 = document.createElement('h1');
 h1.innerText = 'NFT Metadata Generator';
@@ -13,36 +18,47 @@ const subContainer2 = document.createElement('div')
 subContainer2.className = 'subContainer2'
 
 const image = document.createElement('div')
-const title = document.createElement('div')
+const nameDiv = document.createElement('div')
 const description = document.createElement('div')
+const attributes = document.createElement('div')
 const imageFormWrapper = document.createElement('div')
 const imageForm = document.createElement('form')
 const imageTextArea = document.createElement('textarea')
-const titleFormWrapper = document.createElement('div')
-const titleForm = document.createElement('form')
-const titleTextArea = document.createElement('textarea')
+const nameFormWrapper = document.createElement('div')
+const nameForm = document.createElement('form')
+const nameTextArea = document.createElement('textarea')
 const descriptionFormWrapper = document.createElement('div')
 const descriptionForm = document.createElement('form')
 const descriptionTextArea = document.createElement('textarea')
+const attributesFormWrapper = document.createElement('div')
+const attributesForm = document.createElement('form')
+const attributesTextArea = document.createElement('textarea')
+const jsonDisplay = document.createElement('div')
 
-image.innerText = 'ipfs hash:'
-title.innerText = 'title:'
+image.innerText = 'link to image: (i.e. ipfs.io/ipfs/<hash>/<x>.png)'
+nameDiv.innerText = 'name:'
 description.innerText = 'description:'
+attributes.innerText = 'attributes: (i.e. <color: red, size: large>)'
+attributes.className = 'attributes'
 imageFormWrapper.className = 'imageFormWrapper'
 imageTextArea.className = 'imageTextArea'
-titleFormWrapper.className = 'titleFormWrapper'
-titleTextArea.className = 'titleTextArea'
+nameFormWrapper.className = 'nameFormWrapper'
+nameTextArea.className = 'nameTextArea'
 descriptionFormWrapper.className = 'descriptionFormWrapper'
 descriptionTextArea.className = 'descriptionTextArea'
+attributesFormWrapper.className = 'attributesFormWrapper'
+attributesTextArea.className = 'attributesTextArea'
+jsonDisplay.className = 'jsonDisplay'
+jsonDisplay.innerText = JSON.stringify(jsonObject)
 
 document.body.appendChild(h1)
 document.body.appendChild(container)
 container.appendChild(subContainer1)
 container.appendChild(subContainer2)
-subContainer1.appendChild(title)
-subContainer1.appendChild(titleFormWrapper)
-titleFormWrapper.appendChild(titleForm)
-titleForm.appendChild(titleTextArea)
+subContainer1.appendChild(nameDiv)
+subContainer1.appendChild(nameFormWrapper)
+nameFormWrapper.appendChild(nameForm)
+nameForm.appendChild(nameTextArea)
 subContainer1.appendChild(image)
 subContainer1.appendChild(imageFormWrapper)
 imageFormWrapper.appendChild(imageForm)
@@ -51,15 +67,56 @@ subContainer1.appendChild(description)
 subContainer1.appendChild(descriptionFormWrapper)
 descriptionFormWrapper.appendChild(descriptionForm)
 descriptionForm.appendChild(descriptionTextArea)
+subContainer1.appendChild(attributes)
+subContainer1.appendChild(attributesFormWrapper)
+attributesFormWrapper.appendChild(attributesForm)
+attributesForm.appendChild(attributesTextArea)
+subContainer2.appendChild(jsonDisplay)
 
 descriptionTextArea.addEventListener("input", (e) => {
-    // descriptionTextArea.value
+    console.log(descriptionTextArea.value)
+    jsonObject.description = descriptionTextArea.value
+    jsonDisplay.innerText = JSON.stringify(jsonObject)
 })
 
 imageTextArea.addEventListener("input", (e) => {
-    // imageTextArea.value
+    jsonObject.image = imageTextArea.value
+    jsonDisplay.innerText = JSON.stringify(jsonObject)
 })
 
-titleTextArea.addEventListener("input", (e) => {
-    // titleTextArea.value
+nameTextArea.addEventListener("input", (e) => {
+    jsonObject.name = nameTextArea.value
+    jsonDisplay.innerText = JSON.stringify(jsonObject)
+})
+
+attributesTextArea.addEventListener("input", (e) => {
+    let string = attributesTextArea.value
+    let length = attributesTextArea.value.length
+    let res = []
+    let tempObj = {}
+    let key = ''
+    let value = ''
+    let kOrV = true
+    if (length > 0) {
+        for (let i = 0; i < length; i++) {
+            console.log(key)
+            if (string[i] == ',') {
+                tempObj[key] = value
+                kOrV = !kOrV
+                res.push(tempObj)
+                key = ''
+                value = ''
+                tempObj = {}
+                i++
+            } else if (string[i] == ':') {
+                tempObj[key] = null
+                kOrV = !kOrV
+                i++
+            } else {
+                kOrV ? key += string[i] : value += string[i]
+            }
+        }
+        jsonObject.attributes = res
+        jsonDisplay.innerText = JSON.stringify(jsonObject)
+    }
 })
